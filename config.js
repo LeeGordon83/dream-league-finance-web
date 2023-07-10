03158a062873e3a717803bb36d46a16fa5804e1a
LeeGordon83 <lee.gordon83@gmail.com>
2023-07-10 11:37:09 +0100

    initial app setup

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  development: {
+    username: 'your_username',
+    password: 'your_password',
+    database: 'your_database',
+    host: 'localhost',
+    dialect: 'postgres',
+  },
+  production: {
+    // Production database configuration
+    // Adjust these according to your production database settings
+    username: 'your_username',
+    password: 'your_password',
+    database: 'your_database',
+    host: 'localhost',
+    dialect: 'postgres',
+  },
+};
