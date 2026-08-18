f81e6ab4cbb4ffd815032da388894fe1d776b705
LeeGordon83 <lee.gordon83@gmail.com>
2026-08-18 16:23:06 +0100

    made weekly update work

--- app/routes/admin/weekly-update.js
+++ app/routes/admin/weekly-update.js
@@ -1,4 +1,5 @@
 const api = require('../../api')
+const joi = require('joi')
 const { isInRole } = require('../../auth')
 
 const requireAdmin = (request, h) => {
@@ -15,6 +16,7 @@ const emptyUpdate = {
 	latestRecordedWeek: null,
 	weeklyWinners: [],
 	jackpotWinners: [],
+	alreadyRecorded: false,
 	hasWeeklyForCurrentWeek: null
 }
 
@@ -33,6 +35,32 @@ module.exports = [{
 				update: emptyUpdate,
 				error: { message: 'Could not load latest weekly winners right now.' }
 			})
+		}
+	}
+}, {
+	method: 'POST',
+	path: '/admin/weekly-update',
+	config: {
+		pre: [requireAdmin],
+		validate: {
+			payload: joi.object({
+				weekId: joi.number().integer().min(1).required()
+			}),
+			failAction: (_request, h, _error) => h.redirect('/admin/weekly-update').takeover()
+		}
+	},
+	handler: async (request, h) => {
+		try {
+			await api.post('/admin/weekly-update', { weekId: request.payload.weekId }, request.dl_token)
+			return h.redirect('/admin/weekly-update')
+		} catch (error) {
+			const update = await api.get('/admin/weekly-update', request.dl_token).catch(() => emptyUpdate)
+			return h.view('admin/weekly-update', {
+				update,
+				error: {
+					message: error?.data?.payload?.error || 'Could not record weekly winnings right now.'
+				}
+			}).code(400)
 		}
 	}
 }]
